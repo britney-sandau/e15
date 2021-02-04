a3e6ae0827d8597768484fb23463c71e9bafb959
Britney Sandau <britsmitt@gmail.com>
2021-02-04 17:35:40 -0500

    Added example dir

---
+++ practice/example/sample.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang='en'>
+<head>
+    <title>Sample Page</title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+</head>
+<body>
+    <h1>Sample Page</h1>
+</body>
+</html>
