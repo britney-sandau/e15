01ed8993d81f73cf8be39563c9a18adb3441edc1
Britney Sandau <britsmitt@gmail.com>
2021-02-11 00:07:11 -0500

    Added changes from Mac Baby.

--- practice/index.php
+++ practice/index.php
@@ -8,5 +8,12 @@
 <body>
     <h1>Practice Application</h1>
     <p>Added a line from Mac Baby</p>
+
+<ul>
+    <li><a href='https://en.wikipedia.org/wiki/Wizard%27s_First_Rule'>Wizard's First Rule</li>
+    <li><a href='https://en.wikipedia.org/wiki/Outlander_(book_series)'>Outlander</li>
+    <li><a href='https://en.wikipedia.org/wiki/Jurassic_Park_(novel)'>Jurassic Park</li>
+</ul>
+
 </body> 
 </html>
