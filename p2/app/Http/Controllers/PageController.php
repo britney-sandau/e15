2395ff4f115dd5456339c8ab91ed39335de9042c
Britney Sandau <britsmitt@gmail.com>
2021-04-01 04:59:49 -0400

    working draft alerts work.

--- p2/app/Http/Controllers/PageController.php
+++ p2/app/Http/Controllers/PageController.php
@@ -10,6 +10,33 @@ class PageController extends Controller
     public function index()
     {
         return view('/welcome');
+
+        // $name = session('name', null);
+        // $sandwich = session('sandwich', null);
+        // $topping1 = session('topping1', null);
+        // $topping2 = session('topping2', null);
+        // $topping3 = session('topping3', null);
+        // $topping4 = session('topping4', null);
+        // $topping5 = session('topping5', null);
+        // $topping6 = session('topping6', null);
+        // $topping7 = session('topping7', null);
+        // $topping8 = session('topping8', null);
+
+
+
+        // return view('/order', [
+        //     'name' => $name,
+        //     'sandwich' => $sandwich,
+        //     'topping1' => $topping1,
+        //     'topping2' => $topping2,
+        //     'topping3' => $topping3,
+        //     'topping4' => $topping4,
+        //     'topping5' => $topping5,
+        //     'topping6' => $topping6,
+        //     'topping7' => $topping7,
+        //     'topping8' => $topping8,
+        //     ]);
+
         
         // $orderResults = session('orderResults', null);
         // return view('welcome', ['orderResults' => $orderResults]);
@@ -20,18 +47,26 @@ class PageController extends Controller
     **/
     public function order(Request $request)
     {
-        $sandwich = $request->input('sandwich', '');
-        $name = $request->input('name', '');
-        $topping1 = $request->input('topping1', '');
-        $topping2 = $request->input('topping2', '');
-        $topping3 = $request->input('topping3', '');
-        $topping4 = $request->input('topping4', '');
-        $topping5 = $request->input('topping5', '');
-        $topping6 = $request->input('topping6', '');
-        $topping7 = $request->input('topping7', '');
-        $topping8 = $request->input('topping8', '');
+        $sandwich = $request->input('sandwich', null);
+        $name = $request->input('name', null);
+        $topping1 = $request->input('topping1', null);
+        $topping2 = $request->input('topping2', null);
+        $topping3 = $request->input('topping3', null);
+        $topping4 = $request->input('topping4', null);
+        $topping5 = $request->input('topping5', null);
+        $topping6 = $request->input('topping6', null);
+        $topping7 = $request->input('topping7', null);
+        $topping8 = $request->input('topping8', null);
+        $topping9 = $request->input('topping9', null);
 
 
+        $data = array($sandwich, $name, $topping1, $topping2);
+
+        foreach ($data as $value) {
+            echo gettype($value), "\n";
+        }
+
+        var_dump($value);
 
 
         //$topping = $request->input('checkbox', '');
@@ -45,6 +80,19 @@ class PageController extends Controller
 
         //return view('/order') . ('Hi ' . $customerName . ', your order of a ' . $sandwich . ' with ' . $topping . ' will be ready in 15 mins.');
         // return view('/order', ['name' => $name], ['sandwich' => $sandwich], ['topping[]' => $topping ?? '']);
+        
+        // return redirect('/order')->with([
+        //     'name' => $name,
+        //     'sandwich' => $sandwich,
+        //     'topping1' => $topping1,
+        //     'topping2' => $topping2,
+        //     'topping3' => $topping3,
+        //     'topping4' => $topping4,
+        //     'topping5' => $topping5,
+        //     'topping6' => $topping6,
+        //     'topping7' => $topping7,
+        //     'topping8' => $topping8,
+        // ]);
         return view('/order', [
             'name' => $name,
             'sandwich' => $sandwich,
@@ -56,7 +104,7 @@ class PageController extends Controller
             'topping6' => $topping6,
             'topping7' => $topping7,
             'topping8' => $topping8,
-            
+            'topping9' => $topping9,
             ]);
 
         //return redirect('/order')->with(['name' => $name]);
--- p2/resources/views/order.blade.php
+++ p2/resources/views/order.blade.php
@@ -12,13 +12,17 @@ Burger Bytes
         <h1>Burger Bytes</h1>
         <h2>Place order</h2>
         <fieldset>
-            <input type='radio' name='sandwich' id='burger' value='Cheeseburger'>
+            <input type='radio' name='sandwich' id='burger' value='Cheeseburger' {{ ($sandwich == 'Cheeseburger' OR is_null($sandwich)) ? 'checked' : ''}}>
             <label for='burger'>Cheeseburger</label>
 
-            <input type='radio' name='sandwich' id='chicken' value='Grilled Chicken'>
+            <input type='radio' name='sandwich' id='chicken' value='Grilled Chicken' {{ ($sandwich == 'Grilled Chicken') ? 'checked' : ''}}>
+
+
             <label for='chicken'>Grilled Chicken</label>
 
-            <input type='radio' name='sandwich' id='veggie' value='Veggie Burger'>
+            <input type='radio' name='sandwich' id='veggie' value='Veggie Burger' {{ ($sandwich == 'Veggie Burger') ? 'checked' : ''}}>
+
+
             <label for='veggie'>Veggie Burger</label>
         </fieldset>
 
@@ -27,39 +31,36 @@ Burger Bytes
         </fieldset>
 
         <fieldset>
-            <input type='checkbox' name='topping1' id='cheese' value='No Cheese'>
-
+            <input type='checkbox' name='topping1' id='cheese' value='No Cheese' {{ ($topping1 == 'No Cheese') ? 'checked' : ''}}>
             <label for='cheese'>No Cheese</label>
 
-            <input type='checkbox' name='topping2' id='tomato' value='No Tomatoes'>
-
+            <input type='checkbox' name='topping2' id='tomato' value='No Tomatoes' {{ ($topping2 == 'No Tomatoes') ? 'checked' : ''}}>
             <label for='tomato'>No Tomatoes</label>
 
-            <input type='checkbox' name='topping3' id='lettuce' value='No Lettuce'>
-
+            <input type='checkbox' name='topping3' id='lettuce' value='No Lettuce' {{ ($topping3 == 'No Lettuce') ? 'checked' : ''}}>
             <label for='lettuce'>No Lettuce</label>
 
-            <input type='checkbox' name='topping4' id='pickles' value='No Pickles'>
-
+            <input type='checkbox' name='topping4' id='pickles' value='No Pickles' {{ ($topping4 == 'No Pickles') ? 'checked' : ''}}>
             <label for='pickles'>No Pickles</label>
 
-            <input type='checkbox' name='topping5' id='onions' value='No Onions'>
-
+            <input type='checkbox' name='topping5' id='onions' value='No Onions' {{ ($topping5 == 'No Onions') ? 'checked' : ''}}>
             <label for='onions'>No Onions</label>
 
-            <input type='checkbox' name='topping6' id='sauce' value='No Special Sauce'>
-
+            <input type='checkbox' name='topping6' id='sauce' value='No Special Sauce' {{ ($topping6 == 'No Special Sauce') ? 'checked' : ''}}>
             <label for='sauce'>No Special Sauce</label>
         </fieldset>
 
-
-        {{-- <fieldset>
-            <input type='checkbox' name='ketchup' id='ketchup' value='Add Ketchup'>
+        <fieldset>
+            <input type='checkbox' name='topping7' id='ketchup' value='Add Ketchup' {{ ($topping7 == 'Add Ketchup') ? 'checked' : ''}}>
             <label for='ketchup'>Add Ketchup</label>
 
-            <input type='checkbox' name='mustard' id='mustard' value='Add mustard'>
+            <input type='checkbox' name='topping8' id='mustard' value='Add Mustard' {{ ($topping8 == 'Add Mustard') ? 'checked' : ''}}>
             <label for='mustard'>Add Mustard</label>
-        </fieldset> --}}
+
+            <input type='checkbox' name='topping9' id='mayo' value='Add Mayo' {{ ($topping9 == 'Add Mayo') ? 'checked' : ''}}>
+            <label for='mustard'>Add Mayo</label>
+
+        </fieldset>
 
         <fieldset>
             <label for='name'>Name:</label>
@@ -94,8 +95,18 @@ Burger Bytes
 
     </form>
 
-    @if(isset($name))
-    <div>
+    @if(is_null($name))
+    <div class='results alert alert-success'>
+        <p>Place your order here.</p>
+    </div>
+    @endif
+
+    @if(!is_string($name))
+    <div class='results alert alert-warning'>
+        <p>Please add your name.</p>
+    </div>
+    @else
+    <div class='results alert alert-primary'>
         <p>Hello {{$name}}</p>
         <p>Your {{$sandwich}} order </p>
         <p>{{$topping1 ?? ''}}</p>
@@ -104,18 +115,113 @@ Burger Bytes
         <p>{{$topping4 ?? ''}}</p>
         <p>{{$topping5 ?? ''}}</p>
         <p>{{$topping6 ?? ''}}</p>
+        <p>{{$topping7 ?? ''}}</p>
+        <p>{{$topping8 ?? ''}}</p>
+        <p>{{$topping9 ?? ''}}</p>
+
 
         <p>will be ready in 15 mins.</p>
-
     </div>
     @endif
 
 
-    <?php
+
+
+
+
+
+
+
+
+
+
+    {{-- @if(!is_null($name))
+    <div>
+        <p>Hello {{$name}}</p>
+    <p>Your {{$sandwich}} order </p>
+    <p>{{$topping1 ?? ''}}</p>
+    <p>{{$topping2 ?? ''}}</p>
+    <p>{{$topping3 ?? ''}}</p>
+    <p>{{$topping4 ?? ''}}</p>
+    <p>{{$topping5 ?? ''}}</p>
+    <p>{{$topping6 ?? ''}}</p>
+    <p>{{$topping7 ?? ''}}</p>
+    <p>{{$topping8 ?? ''}}</p>
+    <p>will be ready in 15 mins.</p>
+</div>
+@else
+<div class='results alert alert-warning'>
+    <p>Please enter your name.</p>
+</div>
+@endif --}}
+
+
+{{--
+@if(!is_string($name))
+<div class='results alert alert-success'>
+    <p>Place your order here.</p>
+</div>
+
+@if(is_string($name))
+<div class='results alert alert-primary'>
+    <p>Hello {{$name}}</p>
+<p>Your {{$sandwich}} order </p>
+<p>{{$topping1 ?? ''}}</p>
+<p>{{$topping2 ?? ''}}</p>
+<p>{{$topping3 ?? ''}}</p>
+<p>{{$topping4 ?? ''}}</p>
+<p>{{$topping5 ?? ''}}</p>
+<p>{{$topping6 ?? ''}}</p>
+
+<p>will be ready in 15 mins.</p>
+</div>
+@else
+
+
+@endif
+<div class='results alert alert-warning'>
+    <p>Please add your name.</p>
+</div>
+
+@endif --}}
+
+
+<?php
 dump($name, $sandwich, $topping1 ?? '')
 ?>
 
-    {{-- @if(!is_null($orderResults))
+
+
+
+
+
+
+
+{{-- @else
+<div class='results alert alert-primary'>
+    <p>Hello {{$name}}</p>
+<p>Your {{$sandwich}} order </p>
+<p>{{$topping1 ?? ''}}</p>
+<p>{{$topping2 ?? ''}}</p>
+<p>{{$topping3 ?? ''}}</p>
+<p>{{$topping4 ?? ''}}</p>
+<p>{{$topping5 ?? ''}}</p>
+<p>{{$topping6 ?? ''}}</p>
+
+<p>will be ready in 15 mins.</p>
+
+</div> --}}
+{{-- @else
+<div class='results alert alert-warning'>
+
+    <p>Please add your name.</p>
+</div> --}}
+
+
+
+
+
+{{-- @if(!is_null($orderResults))
 
   @if(count($orderResults) == 0)
 
@@ -127,14 +233,14 @@ dump($name, $sandwich, $topping1 ?? '')
 
       {{ count($orderResults) }}
 
-    {{ Str::plural('Result', count($orderResults)) }}:
+{{ Str::plural('Result', count($orderResults)) }}:
 
 
-    <ul>
-        @foreach($searchResults as $slug => $book)
-        <li><a href='/books/{{ $slug }}'> {{ $book['title']   }}</a></li>
-        @endforeach
-    </ul>
+<ul>
+    @foreach($searchResults as $slug => $book)
+    <li><a href='/books/{{ $slug }}'> {{ $book['title']   }}</a></li>
+    @endforeach
+</ul>
 </div>
 @endif
 @endif --}}
